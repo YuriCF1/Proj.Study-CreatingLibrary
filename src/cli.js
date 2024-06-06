323e216002a51acf2bd6a44d782c86e4dc4bd8d6
Yuri Cruz <yuricruzf@gmail.com>
2024-06-06 20:08:34 -0300

    Using async functions

--- src/cli.js
+++ src/cli.js
@@ -17,13 +17,39 @@ fs.readFile(linkArquivo, "utf-8", (erro, texto) => {
   }
 });
 
-async function criaESalvaArquivo(listaPalavra, endereco) {
+// async function criaESalvaArquivo(listaPalavra, endereco) {
+//   const arquivoNovo = `${endereco}/resultado.txt`;
+//   const textoPalavras = JSON.stringify(listaPalavra);
+//   try {
+//     await fs.promises.writeFile(arquivoNovo, textoPalavras);
+//     console.log("Arquivo Criado");
+//   } catch (erro) {
+//     throw erro;
+//   }
+// }
+
+function criaESalvaArquivo(listaPalavra, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
   const textoPalavras = JSON.stringify(listaPalavra);
-  try {
-    await fs.promises.writeFile(arquivoNovo, textoPalavras);
-    console.log("Arquivo Criado");
-  } catch (erro) {
-    throw erro;
-  }
+  fs.promises
+    .writeFile(arquivoNovo, textoPalavras) //writefile não retorna nada
+    .then(() => {
+      //then é o responsável para dar a conclusão de uma promise, que pode ter como retorno qualquer tipo de dado
+      console.log("Arquivo criado");
+    })
+    .catch((erro) => {
+      throw erro;
+    })
+    .finally(() => {
+      //Independentemente do resultado que acontece, se for sucesso ou falha, ele acontece. Muito útil para fechar conexões com banco de dados
+      console.log("Operação finalizada");
+    });
 }
+
+/*NOTAS
+
+USOS MAIS COMUNS DE CÓDIGOS ASSÍNCRONOS
+- Leitura/Manipulação de Arquivos em Disco
+- Comunicação entre cliente e servidor
+- Operação em bancos de dados
+*/
